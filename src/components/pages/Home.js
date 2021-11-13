dd570c9437cf506b92d6c0411712f50c4ef32ed7
GKyomen <kenkakyomen@hotmail.com>
2021-11-13 10:01:13 -0300

    Home finished

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,19 @@
+import LinkButton from '../layout/LinkButton'
+import styles from './Home.module.css'
+import dataImg from '../../img/data-science.png'
+
 function Home() {
   return (
-    <section>
+    <section className={styles.home_container}>
       <h1>
         Bem vindo ao <span>Mind Your Data</span>
       </h1>
-      <p>
-        Comece a gerenciar suas informações e a de seus clientes agora mesmo!
-      </p>
+      <p>Comece a gerenciar informações suas e de seus clientes agora mesmo!</p>
+      <LinkButton to="/dashboard" text="Gerenciar dados" />
+      <img
+        src={dataImg}
+        alt="Banco de dados no meio de uma nuvem atômica, representando a ciência de dados"
+      />
     </section>
   )
 }
